9e65ced87c64dd6d1435aca4411daacb23293196
Alfredo Perez <alfredo.perez.q@gmail.com>
2021-04-12 12:27:49 -0500

    Adds moc page

--- src/templates/MocPage.Query.tsx
+++ src/templates/MocPage.Query.tsx
@@ -1,24 +1,24 @@
-// import { graphql } from 'gatsby'
-// import MocPage from '../pages/MocPage'
-//
-// export default MocPage
-//
-// export const query = graphql`
-//   query($slug: String!) {
-//     note(slug: { eq: $slug }) {
-//       slug
-//       title
-//       created
-//       updated
-//       tags {
-//         name
-//         slug
-//       }
-//       description
-//       canonicalUrl
-//       body
-//       excerpt
-//       timeToRead
-//     }
-//   }
-// `
+import { graphql } from 'gatsby'
+import MocPage from '../pages/MocPage'
+
+export default MocPage
+
+export const query = graphql`
+  query($slug: String!) {
+    note(slug: { eq: $slug }) {
+      slug
+      title
+      created
+      updated
+      tags {
+        name
+        slug
+      }
+      description
+      canonicalUrl
+      body
+      excerpt
+      timeToRead
+    }
+  }
+`
