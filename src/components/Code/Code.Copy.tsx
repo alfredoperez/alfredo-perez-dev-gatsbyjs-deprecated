469905e87a344fe276e2bec60944d19c6563a090
Alfredo Perez <alfredo.perez.q@gmail.com>
2021-06-07 16:15:44 -0500

    Moves code-copy component

--- src/components/Code/Code.Copy.tsx
+++ src/components/Code/Code.Copy.tsx
@@ -4,44 +4,4 @@ import { useState } from 'react'
 import { copyToClipboard } from '@utils/copyToClipboard'
 import { visuallyHidden } from '@utils/visuallyHidden'
 
-const delay = (duration: number) => new Promise((resolve) => setTimeout(resolve, duration))
-
-type CodeCopyProps = {
-  content: string
-  duration?: number
-  fileName?: string
-  trim?: boolean
-}
-
-const CodeCopy = ({ content, duration = 5000, fileName = ``, trim = false }: CodeCopyProps) => {
-  const [copied, setCopied] = useState(false)
-
-  const label = copied
-    ? `${fileName ? `${fileName} ` : ``}copied to clipboard`
-    : `${fileName ? `${fileName}: ` : ``}copy code to clipboard`
-
-  return (
-    <button
-      type="button"
-      name={label}
-      disabled={copied}
-      className="code-copy-button"
-      sx={{
-        variant: `copyButton`,
-      }}
-      onClick={async () => {
-        await copyToClipboard(trim ? content.trim() : content)
-        setCopied(true)
-        await delay(duration)
-        setCopied(false)
-      }}
-    >
-      {copied ? `Copied` : `Copy`}
-      <span sx={{ ...visuallyHidden }} aria-roledescription="status">
-        {label}
-      </span>
-    </button>
-  )
-}
-
 export default CodeCopy
