aa239ab7425b5fb0517ab0d15066eb253c11a07b
Alfredo Perez <alfredo.perez.q@gmail.com>
2021-06-08 06:46:43 -0500

    Updates (#19)
    * Auto stash before merge of "master" and "origin/master"
    
    * update gatsby libraries
    
    * Moves code-copy component

--- src/components/Code/Code.tsx
+++ src/components/Code/Code.tsx
@@ -1,10 +1,51 @@
 /* eslint react/destructuring-assignment: 0 */
-import React from 'react'
+import React, { useState } from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import loadable from '@loadable/component'
 import theme from 'prism-react-renderer/themes/shadesOfPurple'
-import CodeCopy from './code.copy'
+import { copyToClipboard } from '@utils/copyToClipboard'
+import { visuallyHidden } from '@utils/visuallyHidden'
 
+const delay = (duration: number) => new Promise((resolve) => setTimeout(resolve, duration))
+
+type CodeCopyProps = {
+  content: string
+  duration?: number
+  fileName?: string
+  trim?: boolean
+}
+
+const CodeCopy = ({ content, duration = 5000, fileName = ``, trim = false }: CodeCopyProps) => {
+  const [copied, setCopied] = useState(false)
+
+  const label = copied
+    ? `${fileName ? `${fileName} ` : ``}copied to clipboard`
+    : `${fileName ? `${fileName}: ` : ``}copy code to clipboard`
+
+  return (
+    <button
+      type="button"
+      name={label}
+      disabled={copied}
+      className="code-copy-button"
+      sx={{
+        variant: `copyButton`,
+      }}
+      onClick={async () => {
+        await copyToClipboard(trim ? content.trim() : content)
+        setCopied(true)
+        await delay(duration)
+        setCopied(false)
+      }}
+    >
+      {copied ? `Copied` : `Copy`}
+      <span sx={{ ...visuallyHidden }} aria-roledescription="status">
+        {label}
+      </span>
+    </button>
+  )
+}
+////
 type CodeProps = {
   codeString: string
   // language: Language
