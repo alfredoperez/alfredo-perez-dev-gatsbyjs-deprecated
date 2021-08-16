90afebd60b56061555c5088ae4a45421096cfc45
Alfredo Perez <alfredo.perez.q@gmail.com>
2021-08-15 19:15:15 -0500

    Update libraries (#22)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import { Layout } from './src/components/Layout/'
+import { Layout } from './src/components/Layout'
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>
